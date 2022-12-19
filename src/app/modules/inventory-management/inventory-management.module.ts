4f780e4d48255d72464c553f4f06487f3e0060e4
Amir Hosein Nazari <czhablog@gmail.com>
2022-12-19 12:19:10 +0330

    feat: fixed minor bugs

--- src/app/modules/inventory-management/inventory-management.module.ts
+++ src/app/modules/inventory-management/inventory-management.module.ts
@@ -4,13 +4,18 @@ import { InventoryManagementRoutingModule } from './inventory-management-routing
 import { InventoryManagementComponent } from './inventory-management.component';
 import { StoreModule } from '@ngrx/store';
 import { reducers } from './store/inventoryManagement.reducers';
+import { ModalsModule, WidgetsModule } from '../../_metronic/partials';
+import { InventoryComponent } from './basic-information/inventory/inventory.component';
 
 @NgModule({
-  declarations: [InventoryManagementComponent],
+  declarations: [InventoryManagementComponent, InventoryComponent],
   imports: [
     CommonModule,
     InventoryManagementRoutingModule,
+    WidgetsModule,
     StoreModule.forFeature('inventoryManagement', reducers),
+    WidgetsModule,
+    ModalsModule,
   ],
 })
 export class InventoryManagementModule {}
